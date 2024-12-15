5f2e47d1ed29f05e07bc2856ea7520968ecbc2d2
litchitsme <karelsegers@outlook.com>
2024-12-15 21:17:59 +0100

    Beetje opgemaakt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+  priority: 'low' | 'medium' | 'high';
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  myImage: string = "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?cs=srgb&dl=pexels-asadphoto-457882.jpg&fm=jpg";
+  number: any = 200;
+  firstName: any;
+  color: any = "red";
+  highlighted: boolean = true;
+  display: boolean = false;
+  sayHello() {
+    alert(this.firstName);
+  }
   title = 'bindings';
 }
